481eeba1bf552e313a2f15686f7afbcbb1a23743
Daniel Mora <daniel_k310a@hotmail.com>
2025-03-24 14:06:42 -0600

    refactor: update age calculation to use short variable declaration

--- 02-variables/main.go
+++ 02-variables/main.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	const thisYear = 2025
 	var name string
-	var age, birthdayYear int
+	var birthdayYear int
 
 	println("Welcome to the profile data app")
 	println("Please enter the following information")
@@ -15,7 +15,8 @@ func main() {
 	println("What year were you born?")
 	fmt.Scanln(&birthdayYear)
 
-	age = thisYear - birthdayYear
+	// USE MATH DECLARATION FOR THIS EXAMPLE
+	age := thisYear - birthdayYear
 
 	fmt.Println("Hi", name, "you are", age, "years old")
 
