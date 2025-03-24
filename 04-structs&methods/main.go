bd8d42f9cbc846929be80f4a14f80281bbf8636c
Daniel Mora <daniel_k310a@hotmail.com>
2025-03-24 16:34:40 -0600

    feat: enhance main program to demonstrate multiple Person instances and age update

--- 04-structs&methods/main.go
+++ 04-structs&methods/main.go
@@ -10,10 +10,18 @@ type Person struct {
 }
 
 func main() {
-
+	// declare new person with struct literal
 	person := Person{name: "Jhon", age: 25, lastName: "Doe", employed: true}
 
 	fmt.Println("Welcome to the profile data app")
 	fmt.Println("This is your information", person)
 	fmt.Println("your name is", person.name)
+
+	// declare new person without struct literal
+	person2 := Person{"Paul", 30, "Smith", false}
+	fmt.Println("This is your information", person2)
+
+	fmt.Println("Update your age")
+	fmt.Scanln(&person2.age)
+	fmt.Println("This is your new information", person2.age)
 }
