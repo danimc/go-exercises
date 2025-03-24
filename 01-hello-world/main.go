f56a2d4d87981af716a25175af007ff84c1469bb
Daniel Mora <daniel_k310a@hotmail.com>
2025-03-24 11:59:30 -0600

    Initial commit

---
+++ 01-hello-world/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, Wordl")
+	profileData()
+}
+
+func profileData() {
+	fmt.Println("Name: Daniel M")
+	fmt.Println("Age: XX")
+	fmt.Println("Location: Mexico")
+}
